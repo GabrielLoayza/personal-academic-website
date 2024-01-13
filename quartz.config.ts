5e3eac7aa998cf069806f704bfceda78160db60d
GabrielLoayza <loayzapg@gmail.com>
2024-01-12 18:50:33 -0700

    Quartz sync: Jan 12, 2024, 6:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gabriel Loayza",
+    pageTitle: "HelixHive 🧬",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
