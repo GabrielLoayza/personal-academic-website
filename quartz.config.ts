fb3f4dfd528a8214d61a8b62bbd8781e6f574f1b
GabrielLoayza <loayzapg@gmail.com>
2024-04-15 19:29:28 -0500

    Quartz sync: Apr 15, 2024, 7:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧬*pdb",
+    pageTitle: "🧬.pdb",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
